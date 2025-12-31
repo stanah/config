ab7acb7f895ca1d78e8c177cff0cdfdcf2e90367
stanah <foo.exe@gmail.com>
2026-01-01 04:37:04 +0900

    nvim-treesitter の API 変更に追従

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -24,6 +24,6 @@ return {
     indent = { enable = true },
   },
   config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+    require("nvim-treesitter").setup(opts)
   end,
 }
