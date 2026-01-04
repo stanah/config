b10d93192bcaae2e451887e6dbe84dc8479c1b01
stanah <foo.exe@gmail.com>
2026-01-04 18:58:27 +0900

    bootstrap: enable nix-command and flakes

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,6 +11,15 @@ fi
 
 nix --version
 
+if [[ "${NIX_CONFIG:-}" != *"experimental-features"* ]]; then
+  if [[ -n "${NIX_CONFIG:-}" ]]; then
+    NIX_CONFIG="${NIX_CONFIG}"$'\n'"experimental-features = nix-command flakes"
+  else
+    NIX_CONFIG="experimental-features = nix-command flakes"
+  fi
+  export NIX_CONFIG
+fi
+
 HOST="${1:-}"
 if [ -z "$HOST" ] && [ -f "./nix/user-config.nix" ]; then
   HOST="$(nix eval --raw --expr '(import ./nix/user-config.nix).host' 2>/dev/null || true)"
